5a710c3199ddd6d8beaba9572a7822a07a01b034
Javidan <cavidan.alizade@cybernet.az>
2022-04-11 13:52:24 +0400

    initial files created

---
+++ src/main/java/com/bakcell/nomre/NomreApplication.java
@@ -0,0 +1,13 @@
+package com.bakcell.nomre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakcell/nomre/NomreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakcell.nomre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
